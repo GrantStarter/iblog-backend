34f375a310421c42c3212f3cf1888249a53b70f9
Jason-WS <cyi18@icloud.com>
2025-01-31 10:11:53 +0800

    initial commit

---
+++ src/test/java/com/iblog/IblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
